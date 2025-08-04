d8cca5dc1c6e04b34a53a62d1e27c93550972790
Naman Srivastava <namansrivastava1608@gmail.com>
2025-08-04 14:23:59 +0530

    Databse set up and pushing orders and users.csv to mongodb

---
+++ database.py
@@ -0,0 +1,37 @@
+import csv
+import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://namansrivastava1608:fxI2C833oZjW66s7@cluster0.ity4hgk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+data="think41"
+collec="user"
+CSV_FILE="users.csv"
+def load_data():
+    client=pymongo.MongoClient(uri)
+    db=client[data]
+    collection=db[collec]
+    csvinsert=[]
+    with open(CSV_FILE,'r',encoding='utf-8') as file:
+        reader=csv.DictReader(file)
+        for row in reader:
+            try:
+                row['id']=row['id']
+                row['first_name']=row['first_name']
+                row['last_name']=row['last_name']
+                row['email']=row['email']
+                row['age']=row['age']
+                row['gender']=row['gender']
+                row['state']=row['state']
+                row['country']=row['country']
+                csvinsert.append(row)
+            except ValueError as e:
+                print(f"Error in data types") #only if i convert
+        if csvinsert:
+            collection.insert_many(csvinsert)
+        else:
+            print("No data to insert")
+
+if __name__=="__main__":
+    load_data()
